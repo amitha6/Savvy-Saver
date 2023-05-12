df78a89dce14be78b19ac752d4629da78b42c5e8
manaswitha2000 <manaswitha.reddy@iiitb.org>
2023-05-12 15:33:07 +0530

    Initial commit

--- Savvy-Saver/urls.py
+++ Savvy-Saver/urls.py
@@ -1,4 +1,4 @@
-"""SuperMarkIt URL Configuration
+"""Savvy-Saver URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SuperMarkIt.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Savvy-Saver.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
